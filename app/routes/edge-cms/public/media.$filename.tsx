72260a0a6efdbaf74c529f5f09bd9eee27961b49
Rodrigo Solís <solis.ro@gmail.com>
2025-08-05 10:22:38 +0200

    fix: add expires header

--- app/routes/edge-cms/public/media.$filename.tsx
+++ app/routes/edge-cms/public/media.$filename.tsx
@@ -14,8 +14,10 @@ export async function loader({ params }: Route.LoaderArgs) {
   const headers = new Headers();
   object.writeHttpMetadata(headers);
   headers.set("etag", object.httpEtag);
-  headers.set("Cache-Control", "public, max-age=86400, stale-while-revalidate=604800"); // 24 hour cache, 7 day stale
+  headers.set("Cache-Control", "public, max-age=172800, stale-while-revalidate=604800"); // 48 hour cache, 7 day stale
   headers.set("Accept-Ranges", "bytes");
+  // 48 hour cache in GMT
+  headers.set("Expires", new Date(Date.now() + 172800 * 1000).toUTCString());
 
   return new Response(object.body, {
     headers,
